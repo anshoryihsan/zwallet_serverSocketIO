d5e48e03a4b45d4ac98fac0f860436e05c8e7c73
anshoryihsan <anshoryihsan@.gmail.com>
2020-12-14 18:00:21 +0700

    update

--- index.js
+++ index.js
@@ -32,27 +32,27 @@ io.on("connection", (socket) => {
   const id = socket.handshake.query.id;
   socket.join(id);
   console.log(`user ${id} connected`);
-  db.query(`SELECT balance FROM profile where id=${id}`, (err, data) => {
-    if (!err) {
-      io.to(id).emit("get_info_balance", data);
-    } else {
-      console.log(err);
-    }
-  });
-
-  // socket.on("info_balance", ({ id }) => {
-  //   // console.log(id);
-  //   if (id) {
-  //     db.query(`SELECT balance FROM profile where id=${id}`, (err, data) => {
-  //       if (!err) {
-  //         // console.log(data);
-  //         // socket.join(id);
-  //         io.to(id).emit("get_info_balance", data);
-  //       } else {
-  //         console.log(err);
-  //       }
-  //     });
+  // db.query(`SELECT balance FROM profile where id=${id}`, (err, data) => {
+  //   if (!err) {
+  //     io.to(id).emit("get_info_balance", data);
+  //   } else {
+  //     console.log(err);
   //   }
+  // });
+
+  // socket.on("info_balance", (id) => {
+  // console.log(id);
+  if (id) {
+    db.query(`SELECT balance FROM profile where id=${id}`, (err, data) => {
+      if (!err) {
+        // console.log(data);
+        // socket.join(id);
+        io.to(id).emit("get_info_balance", data);
+      } else {
+        console.log(err);
+      }
+    });
+  }
   // });
 
   // io.to(id).emit("transaction", { title: " message " });
